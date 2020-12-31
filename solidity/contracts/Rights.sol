feb7d5e04dc9c57efca0c18bec1c8c2d66adc94a
Satoshi Amemiya <amemiya@protonmail.com>
2021-01-01 01:34:33 +0900

    wip

--- solidity/contracts/Rights.sol
+++ solidity/contracts/Rights.sol
@@ -10,15 +10,28 @@ contract Rights is ERC721 {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
+  struct Token {
+    uint256 id;
+    string currentName;
+    uint256 expire;
+  }
+  mapping(uint256 => Token) private _tokenMap;
+
   constructor() ERC721("Namer", "NAMER") {}
 
-  function publish(address author) public returns (uint256) {
-    require(msg.sender == address(new Tokens()));
+  function publish(address author, uint256 origin, uint256 duration) public returns (uint256) {
+    // require(msg.sender == address(new Tokens()));
+
+    uint256 now = block.timestamp;
+    Token memory existing = _tokenMap[origin];
+    require(existing.expire > now);
+
     _tokenIds.increment();
     uint256 newItemId = _tokenIds.current();
 
-    _mint(author, newItemId);
+    _tokenMap[origin] = Token(newItemId, "", now + duration);
 
+    _mint(msg.sender, newItemId);
     return newItemId;
   }
 }
--- solidity/contracts/Tokens.sol
+++ solidity/contracts/Tokens.sol
@@ -6,37 +6,24 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./Rights.sol";
 
 contract Tokens is ERC721 {
-  struct LendingInfo {
-    uint256 right;
-    uint256 until;
-  }
-
   struct Token {
     string defaultName;
-    LendingInfo lending;
+    string url;
   }
   Token[] private _tokens;
 
-  constructor() ERC721("Namer", "NAMER") {}
+  constructor() ERC721("Blank", "BLANK") {}
 
   function lent(uint256 tokenId, uint64 duration) public {
-    Token memory token = _tokens[tokenId];
-    uint256 now = block.timestamp;
-
-    require(token.lending.until < now);
-    uint256 until = now + duration;
     Rights Rights = new Rights();
-    uint256 right = Rights.publish(msg.sender);
-    token.lending = LendingInfo(right, until);
+    Rights.publish(msg.sender, tokenId, duration);
   }
 
-  function publish(string calldata uri, string calldata defaultName) public returns (uint256) {
-    _tokens.push(Token(defaultName, LendingInfo(0, 0)));
+  function publish(string calldata url, string calldata defaultName) public returns (uint256) {
+    _tokens.push(Token(defaultName, url));
     uint256 newItemId = _tokens.length;
 
     _mint(msg.sender, newItemId);
-    _setTokenURI(newItemId, uri);
-
     return newItemId;
   }
 }
